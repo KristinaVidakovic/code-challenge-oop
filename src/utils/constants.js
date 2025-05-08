e70834edd6a1c745ec055d019f3042152568f990
kristinavd <kristina.vidakovic403@gmail.com>
2025-05-08 19:14:41 +0200

    Remove js files

--- src/utils/constants.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.HORIZONTAL_CHARACTER = exports.VERTICAL_CHARACTER = exports.NO_PATH_CHARACTER = exports.CORNER_CHARACTER = exports.END_CHARACTER = exports.START_CHARACTER = void 0;
-exports.START_CHARACTER = '@';
-exports.END_CHARACTER = 'x';
-exports.CORNER_CHARACTER = '+';
-exports.NO_PATH_CHARACTER = ' ';
-exports.VERTICAL_CHARACTER = '|';
-exports.HORIZONTAL_CHARACTER = '-';
